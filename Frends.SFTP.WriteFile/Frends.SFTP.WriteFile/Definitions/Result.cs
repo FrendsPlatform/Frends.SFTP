6e3513657bc00100296cede324463d674846fd60
Riku Virtanen <virtanen.riku91@gmail.com>
2022-06-15 09:13:12 +0300

    Minor visual changes

--- Frends.SFTP.WriteFile/Frends.SFTP.WriteFile/Definitions/Result.cs
+++ Frends.SFTP.WriteFile/Frends.SFTP.WriteFile/Definitions/Result.cs
@@ -1,30 +1,30 @@
 ﻿using Renci.SshNet.Sftp;
 using System.ComponentModel.DataAnnotations;
 
-namespace Frends.SFTP.WriteFile.Definitions
+namespace Frends.SFTP.WriteFile.Definitions;
+
+/// <summary>
+/// Return object with private setters
+/// </summary>
+public class Result
 {
     /// <summary>
-    /// Return object with private setters
+    /// Full path to the written file.
     /// </summary>
-    public class Result
+    /// <example>/destination/newfile.txt</example>
+    [DisplayFormat(DataFormatString = "Text")]
+	public string Path { get; private set; }
+
+    /// <summary>
+    /// Size of the new file in destination.
+    /// </summary>
+    /// <example>3.2</example>
+    public double SizeInMegaBytes { get; private set; }
+
+    internal Result(SftpFile file)
     {
-        /// <summary>
-        /// Full path to the written file.
-        /// </summary>
-        /// <example>/destination/newfile.txt</example>
-        [DisplayFormat(DataFormatString = "Text")]
-	    public string Path { get; private set; }
-
-        /// <summary>
-        /// Size of the new file in destination.
-        /// </summary>
-        /// <example>3.2</example>
-        public double SizeInMegaBytes { get; private set; }
-
-        internal Result(SftpFile file)
-        {
-            Path = file.FullName;
-            SizeInMegaBytes = file.Length / 1024 / 1024;
-        }
+        Path = file.FullName;
+        SizeInMegaBytes = Math.Round((file.Length / 1024d / 1024d), 3);
     }
 }
+
--- Frends.SFTP.WriteFile/Frends.SFTP.WriteFile/Frends.SFTP.WriteFile.cs
+++ Frends.SFTP.WriteFile/Frends.SFTP.WriteFile/Frends.SFTP.WriteFile.cs
@@ -6,194 +6,194 @@ using System.Text;
 using Frends.SFTP.WriteFile.Definitions;
 using Frends.SFTP.WriteFile.Enums;
 
-namespace Frends.SFTP.WriteFile
+namespace Frends.SFTP.WriteFile;
+
+/// <summary>
+/// Main class for the task.
+/// </summary>
+public class SFTP
 {
     /// <summary>
-    /// Main class for the task.
+    /// Writes string content to a file through SFTP connection.
+    /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends.SFTP.WriteFile)
     /// </summary>
-    public class SFTP
+    /// <param name="connection">Transfer connection parameters</param>
+    /// <param name="input">Write options with full path and string content</param>
+    /// <param name="cancellationToken">CancellationToken is given by Frends</param>
+    /// <returns>Result object {string Path, double SizeInMegaBytes} </returns>
+    public static Result WriteFile([PropertyTab] Input input, [PropertyTab] Connection connection, CancellationToken cancellationToken)
     {
-        /// <summary>
-        /// Writes string content to a file through SFTP connection.
-        /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends.SFTP.WriteFile)
-        /// </summary>
-        /// <param name="connection">Transfer connection parameters</param>
-        /// <param name="input">Write options with full path and string content</param>
-        /// <param name="cancellationToken">CancellationToken is given by Frends</param>
-        /// <returns>Result object {bool Success, string Path, double SizeInMegaBytes} </returns>
-        public static Result WriteFile([PropertyTab] Input input, [PropertyTab] Connection connection, CancellationToken cancellationToken)
-        {
-            var encoding = GetEncoding(input.FileEncoding, input.EnableBom, input.EncodingInString);
+        var encoding = GetEncoding(input.FileEncoding, input.EnableBom, input.EncodingInString);
 
-            ConnectionInfo connectionInfo;
-            // Establish connectionInfo with connection parameters
+        ConnectionInfo connectionInfo;
+        // Establish connectionInfo with connection parameters
+        try
+        {
+            connectionInfo = GetConnectionInfo(input, connection);
+            connectionInfo.Encoding = encoding;
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException($"Error when initializing connection info: {e}");
+        }
+
+        using var client = new SftpClient(connectionInfo);
+
+        //Disable support for these host key exchange algorithms relating: https://github.com/FrendsPlatform/Frends.SFTP/security/dependabot/4
+        client.ConnectionInfo.KeyExchangeAlgorithms.Remove("curve25519-sha256");
+        client.ConnectionInfo.KeyExchangeAlgorithms.Remove("curve25519-sha256@libssh.org");
+
+        // Check the fingerprint of the server if given.
+        if (!String.IsNullOrEmpty(connection.ServerFingerPrint))
+        {
+            var userResultMessage = "";
             try
             {
-                connectionInfo = GetConnectionInfo(input, connection);
-                connectionInfo.Encoding = encoding;
-            }
-            catch (Exception e)
-            {
-                throw new ArgumentException($"Error when initializing connection info: {e}");
-            }
-
-            using var client = new SftpClient(connectionInfo);
-
-            //Disable support for these host key exchange algorithms relating: https://github.com/FrendsPlatform/Frends.SFTP/security/dependabot/4
-            client.ConnectionInfo.KeyExchangeAlgorithms.Remove("curve25519-sha256");
-            client.ConnectionInfo.KeyExchangeAlgorithms.Remove("curve25519-sha256@libssh.org");
-
-            // Check the fingerprint of the server if given.
-            if (!String.IsNullOrEmpty(connection.ServerFingerPrint))
-            {
-                var userResultMessage = "";
-                try
+                // If this check fails then SSH.NET will throw an SshConnectionException - with a message of "Key exchange negotiation failed".
+                client.HostKeyReceived += delegate (object sender, HostKeyEventArgs e)
                 {
-                    // If this check fails then SSH.NET will throw an SshConnectionException - with a message of "Key exchange negotiation failed".
-                    client.HostKeyReceived += delegate (object sender, HostKeyEventArgs e)
+                    // First try with SHA256 typed fingerprint
+                    using (SHA256 mySHA256 = SHA256.Create())
                     {
-                        // First try with SHA256 typed fingerprint
-                        using (SHA256 mySHA256 = SHA256.Create())
-                        {
-                            var sha256Fingerprint = Convert.ToBase64String(mySHA256.ComputeHash(e.HostKey));
-                            // Set the userResultMessage in case checking server fingerprint failes.
-                            userResultMessage = $"Can't trust SFTP server. The server fingerprint does not match. " +
-                                                    $"Expected fingerprint: '{connection.ServerFingerPrint}', but was: '{sha256Fingerprint}'";
-                            e.CanTrust = (sha256Fingerprint == connection.ServerFingerPrint);
-                        }
+                        var sha256Fingerprint = Convert.ToBase64String(mySHA256.ComputeHash(e.HostKey));
+                        // Set the userResultMessage in case checking server fingerprint failes.
+                        userResultMessage = $"Can't trust SFTP server. The server fingerprint does not match. " +
+                                                $"Expected fingerprint: '{connection.ServerFingerPrint}', but was: '{sha256Fingerprint}'";
+                        e.CanTrust = (sha256Fingerprint == connection.ServerFingerPrint);
+                    }
 
-                        if (!e.CanTrust)
-                        {
+                    if (!e.CanTrust)
+                    {
 
-                            userResultMessage = $"Can't trust SFTP server. The server fingerprint does not match. " +
-                                                    $"Expected fingerprint: '{connection.ServerFingerPrint}', but was: '{BitConverter.ToString(e.FingerPrint).Replace("-", ":")}'";
-                            // If previous failed try with MD5 typed fingerprint
-                            var expectedFingerprint = ConvertFingerprintToByteArray(connection.ServerFingerPrint);
-                            e.CanTrust = e.FingerPrint.SequenceEqual(expectedFingerprint);
-                        }
+                        userResultMessage = $"Can't trust SFTP server. The server fingerprint does not match. " +
+                                                $"Expected fingerprint: '{connection.ServerFingerPrint}', but was: '{BitConverter.ToString(e.FingerPrint).Replace("-", ":")}'";
+                        // If previous failed try with MD5 typed fingerprint
+                        var expectedFingerprint = ConvertFingerprintToByteArray(connection.ServerFingerPrint);
+                        e.CanTrust = e.FingerPrint.SequenceEqual(expectedFingerprint);
+                    }
 
-                    };
-                }
-                catch
-                {
-                    throw new ArgumentException($"Error when checking the server fingerprint: {userResultMessage}");
-                }
+                };
             }
-
-            client.ConnectionInfo.Timeout = TimeSpan.FromSeconds(connection.ConnectionTimeout);
-
-            client.BufferSize = connection.BufferSize * 1024;
-
-            client.Connect();
-
-            cancellationToken.ThrowIfCancellationRequested();
-
-            if (!client.IsConnected) throw new ArgumentException($"Error while connecting to destination: {connection.Address}");
-            switch (input.WriteBehaviour)
+            catch
             {
-                case WriteOperation.Append:
-                    var content = "\n" + input.Content;
-                    client.AppendAllText(input.Path, content, encoding);
-                    break;
-                case WriteOperation.Overwrite:
-                    client.WriteAllText(input.Path, input.Content, encoding);
-                    break;
-                case WriteOperation.Error:
-                    if (client.Exists(input.Path))
-                        throw new ArgumentException($"File already exists: {input.Path}");
-                    client.WriteAllText(input.Path, input.Content, encoding);
-                    break;
-                default:
-                    throw new ArgumentException($"Unknown WriteBehaviour type: '{input.WriteBehaviour}'.");
+                throw new ArgumentException($"Error when checking the server fingerprint: {userResultMessage}");
             }
-            var result = new Result(client.Get(input.Path));
-
-            client.Disconnect();
-            client.Dispose();
-
-            return result;
         }
 
-        #region Helper methods
-        private static ConnectionInfo GetConnectionInfo(Input input, Connection connect)
+        client.ConnectionInfo.Timeout = TimeSpan.FromSeconds(connection.ConnectionTimeout);
+
+        client.BufferSize = connection.BufferSize * 1024;
+
+        client.Connect();
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (!client.IsConnected) throw new ArgumentException($"Error while connecting to destination: {connection.Address}");
+        switch (input.WriteBehaviour)
         {
-            ConnectionInfo connectionInfo;
-            List<AuthenticationMethod> methods = new List<AuthenticationMethod>();
-            PrivateKeyFile privateKey = null;
-            if (connect.Authentication == AuthenticationType.UsernamePrivateKeyFile || connect.Authentication == AuthenticationType.UsernamePasswordPrivateKeyFile)
-            {
-                if (string.IsNullOrEmpty(connect.PrivateKeyFile))
-                    throw new ArgumentException("Private key file path was not given.");
-                privateKey = (connect.PrivateKeyFilePassphrase != null)
-                    ? new PrivateKeyFile(connect.PrivateKeyFile, connect.PrivateKeyFilePassphrase)
-                    : new PrivateKeyFile(connect.PrivateKeyFile);
-            }
-            if (connect.Authentication == AuthenticationType.UsernamePrivateKeyString || connect.Authentication == AuthenticationType.UsernamePasswordPrivateKeyString)
-            {
-                if (string.IsNullOrEmpty(connect.PrivateKeyString))
-                    throw new ArgumentException("Private key string was not given.");
-                var stream = new MemoryStream(Encoding.UTF8.GetBytes(connect.PrivateKeyString));
-                privateKey = (connect.PrivateKeyFilePassphrase != null)
-                    ? new PrivateKeyFile(stream, connect.PrivateKeyFilePassphrase)
-                    : new PrivateKeyFile(stream, "");
-            }
-            switch (connect.Authentication)
-            {
-                case AuthenticationType.UsernamePassword:
-                    methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
-                    break;
-                case AuthenticationType.UsernamePrivateKeyFile:
-                    methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
-                    break;
-                case AuthenticationType.UsernamePasswordPrivateKeyFile:
-                    methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
-                    methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
-                    break;
-                case AuthenticationType.UsernamePrivateKeyString:
-                    methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
-                    break;
-                case AuthenticationType.UsernamePasswordPrivateKeyString:
-                    methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
-                    methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
-                    break;
-                default:
-                    throw new ArgumentException($"Unknown Authentication type: '{connect.Authentication}'.");
-            }
-
-            connectionInfo = new ConnectionInfo(connect.Address, connect.Port, connect.UserName, methods.ToArray());
-
-            return connectionInfo;
+            case WriteOperation.Append:
+                var content = "\n" + input.Content;
+                client.AppendAllText(input.Path, content, encoding);
+                break;
+            case WriteOperation.Overwrite:
+                client.WriteAllText(input.Path, input.Content, encoding);
+                break;
+            case WriteOperation.Error:
+                if (client.Exists(input.Path))
+                    throw new ArgumentException($"File already exists: {input.Path}");
+                client.WriteAllText(input.Path, input.Content, encoding);
+                break;
+            default:
+                throw new ArgumentException($"Unknown WriteBehaviour type: '{input.WriteBehaviour}'.");
         }
+        var result = new Result(client.Get(input.Path));
 
-        /// <summary>
-        /// Get encoding for the file name to be transferred.
-        /// </summary>
-        private static Encoding GetEncoding(FileEncoding optionsFileEncoding, bool optionsEnableBom, string optionsEncodingInString)
-        {
-            switch (optionsFileEncoding)
-            {
-                case FileEncoding.UTF8:
-                    return optionsEnableBom ? new UTF8Encoding(true) : new UTF8Encoding(false);
-                case FileEncoding.ASCII:
-                    return Encoding.ASCII;
-                case FileEncoding.ANSI:
-                    return Encoding.Default;
-                case FileEncoding.Unicode:
-                    return Encoding.Unicode;
-                case FileEncoding.WINDOWS1252:
-                    return Encoding.Default;
-                case FileEncoding.Other:
-                    return Encoding.GetEncoding(optionsEncodingInString);
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        client.Disconnect();
+        client.Dispose();
 
-        private static byte[] ConvertFingerprintToByteArray(string fingerprint)
-        {
-            return fingerprint.Split(':').Select(s => Convert.ToByte(s, 16)).ToArray();
-        }
-
-        #endregion
+        return result;
     }
+
+    #region Helper methods
+    private static ConnectionInfo GetConnectionInfo(Input input, Connection connect)
+    {
+        ConnectionInfo connectionInfo;
+        List<AuthenticationMethod> methods = new List<AuthenticationMethod>();
+        PrivateKeyFile privateKey = null;
+        if (connect.Authentication == AuthenticationType.UsernamePrivateKeyFile || connect.Authentication == AuthenticationType.UsernamePasswordPrivateKeyFile)
+        {
+            if (string.IsNullOrEmpty(connect.PrivateKeyFile))
+                throw new ArgumentException("Private key file path was not given.");
+            privateKey = (connect.PrivateKeyFilePassphrase != null)
+                ? new PrivateKeyFile(connect.PrivateKeyFile, connect.PrivateKeyFilePassphrase)
+                : new PrivateKeyFile(connect.PrivateKeyFile);
+        }
+        if (connect.Authentication == AuthenticationType.UsernamePrivateKeyString || connect.Authentication == AuthenticationType.UsernamePasswordPrivateKeyString)
+        {
+            if (string.IsNullOrEmpty(connect.PrivateKeyString))
+                throw new ArgumentException("Private key string was not given.");
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(connect.PrivateKeyString));
+            privateKey = (connect.PrivateKeyFilePassphrase != null)
+                ? new PrivateKeyFile(stream, connect.PrivateKeyFilePassphrase)
+                : new PrivateKeyFile(stream, "");
+        }
+        switch (connect.Authentication)
+        {
+            case AuthenticationType.UsernamePassword:
+                methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
+                break;
+            case AuthenticationType.UsernamePrivateKeyFile:
+                methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
+                break;
+            case AuthenticationType.UsernamePasswordPrivateKeyFile:
+                methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
+                methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
+                break;
+            case AuthenticationType.UsernamePrivateKeyString:
+                methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
+                break;
+            case AuthenticationType.UsernamePasswordPrivateKeyString:
+                methods.Add(new PasswordAuthenticationMethod(connect.UserName, connect.Password));
+                methods.Add(new PrivateKeyAuthenticationMethod(connect.UserName, privateKey));
+                break;
+            default:
+                throw new ArgumentException($"Unknown Authentication type: '{connect.Authentication}'.");
+        }
+
+        connectionInfo = new ConnectionInfo(connect.Address, connect.Port, connect.UserName, methods.ToArray());
+
+        return connectionInfo;
+    }
+
+    /// <summary>
+    /// Get encoding for the file name to be transferred.
+    /// </summary>
+    private static Encoding GetEncoding(FileEncoding optionsFileEncoding, bool optionsEnableBom, string optionsEncodingInString)
+    {
+        switch (optionsFileEncoding)
+        {
+            case FileEncoding.UTF8:
+                return optionsEnableBom ? new UTF8Encoding(true) : new UTF8Encoding(false);
+            case FileEncoding.ASCII:
+                return Encoding.ASCII;
+            case FileEncoding.ANSI:
+                return Encoding.Default;
+            case FileEncoding.Unicode:
+                return Encoding.Unicode;
+            case FileEncoding.WINDOWS1252:
+                return Encoding.Default;
+            case FileEncoding.Other:
+                return Encoding.GetEncoding(optionsEncodingInString);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private static byte[] ConvertFingerprintToByteArray(string fingerprint)
+    {
+        return fingerprint.Split(':').Select(s => Convert.ToByte(s, 16)).ToArray();
+    }
+
+    #endregion
 }
+
