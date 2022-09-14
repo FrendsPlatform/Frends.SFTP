b99fcfcd83e692c2b09719bebab419dabcf65bde
Riku Virtanen <virtanen.riku91@gmail.com>
2022-09-14 10:23:14 +0300

    Fixed tests

--- Frends.SFTP.UploadFiles/Frends.SFTP.UploadFiles.Tests/ErrorTesting.cs
+++ Frends.SFTP.UploadFiles/Frends.SFTP.UploadFiles.Tests/ErrorTesting.cs
@@ -41,7 +41,7 @@ class ErrorTesting : UploadFilesTestBase
         connection.Port = 51651;
 
         var ex = Assert.Throws<Exception>(() => SFTP.UploadFiles(_source, _destination, connection, _options, _info, new CancellationToken()));
-        Assert.AreEqual(ex.Message, "SFTP transfer failed: Unable to establish the socket: No connection could be made because the target machine actively refused it.");
+        Assert.That(ex.Message.StartsWith("SFTP transfer failed: Unable to establish the socket:"));
     }
 
     [Test]
@@ -51,7 +51,7 @@ class ErrorTesting : UploadFilesTestBase
         connection.Address = "local";
 
         var ex = Assert.Throws<Exception>(() => SFTP.UploadFiles(_source, _destination, connection, _options, _info, new CancellationToken()));
-        Assert.AreEqual(ex.Message, "SFTP transfer failed: Unable to establish the socket: No such host is known.");
+        Assert.That(ex.Message.StartsWith("SFTP transfer failed: Unable to establish the socket:"));
     }
 
     [Test]
