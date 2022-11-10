9a2fdf4b16f980d97f047a3086913a07d0571a78
Riku Virtanen <virtanen.riku91@gmail.com>
2022-11-10 08:54:00 +0200

    Modified tests and added ConnectionInfoBuilder class

--- Frends.SFTP.WriteFile/Frends.SFTP.WriteFile.Tests/Lib/WriteFileTestBase.cs
+++ Frends.SFTP.WriteFile/Frends.SFTP.WriteFile.Tests/Lib/WriteFileTestBase.cs
@@ -1,9 +1,18 @@
 ﻿using NUnit.Framework;
+using Frends.SFTP.WriteFile.Definitions;
 
 namespace Frends.SFTP.WriteFile.Tests;
 
 public class WriteFileTestBase
 {
+    protected static Input _input;
+
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
     [TearDown]
     public void TearDown()
     { 
